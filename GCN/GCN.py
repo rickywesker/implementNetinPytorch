6c3e4200c47b4abde992db0b55539cb9ce73880a
Rickycs <rickycs97@pku.edu.cn>
2020-09-24 20:08:44 +0800

    model

--- GCN/GCN.py
+++ GCN/GCN.py
@@ -54,3 +54,36 @@ class GraphConvolution(Module):
 
     def __repr__(self):
         return self.__class__.__name__ + '(' + str(self.in_features) + '->' + str(self.out_features) + ')'
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GCN(Module):
+
+    """
+    two layer GCN model
+    ...
+    Attributes
+    -----------
+        - n_feat: int
+        - n_hid: int
+        - n_class: int
+        - dropout: float
+
+    Methods
+    --------
+        - forward(self, x, adj)
+    """
+
+    def __init__(self, n_feat, n_hid, n_class, dropout):
+        super(GCN, self).__init__()
+        self.gc1 = GraphConvolution(n_feat, n_hid)
+        self.gc2 = GraphConvolution(n_hid, n_class)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x,adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
